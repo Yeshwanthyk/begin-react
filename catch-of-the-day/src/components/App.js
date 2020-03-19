d89765367b08b754289d0a8606a4bc056229a951
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-03-19 00:55:34 -0400

    Pass down objects using state and update them

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,23 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  // First we create an empty state
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. create shallow copy of the state to avoid mutations
+    const fishes = { ...this.state.fishes };
+    // 2. Update the shallow copy
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set state with updated values
+    this.setState({
+      fishes: fishes
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +28,8 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood" />
         </div>
         <Order />
-        <Inventory />
+        {/* We pass down the methods downstream as `props` */}
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- catch-of-the-day/src/components/storepicker.js
+++ catch-of-the-day/src/components/storepicker.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  // Instead we can use arroy functions, they will bind to the instance
+
+  goToStore = event => {
+    // Prevent form from resetting the page
+    event.preventDefault();
+
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Pick a store</h2>
-        <input type="text" required placeholder="Store Name"></input>
+        <input
+          type="text"
+          required
+          ref={this.myInput}
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        ></input>
         <button type="submit"> Visit Store -></button>
       </form>
     );
