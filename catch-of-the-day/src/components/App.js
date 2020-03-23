247c498c57567a8019947e27b5167f38fbe8fba7
Yesh  (Keerthi Yendamuri) <yeshwanth.yk@gmail.com>
2020-03-23 17:12:31 -0400

    Make Fish component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
   // First we create an empty state
@@ -31,6 +32,13 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood" />
+          <ul className="fishes">
+            {/* Object.keys allows us to get an array of the fishes so that we can
+            map over it as we cant map over Object (which the state is) */}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
         {/* We pass down the methods downstream as `props` */}
